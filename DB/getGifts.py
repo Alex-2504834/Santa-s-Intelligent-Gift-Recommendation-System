7af02ec9c281d9285e3093b3332821e311c973fc
coneastdev <coneastdev@proton.me>
2025-12-15 13:44:55 +0000

    update get gifts

--- DB/getGifts.py
+++ DB/getGifts.py
@@ -31,32 +31,65 @@ for wish in wishlists:
     wish = wish.replace("]", "")
     gifts:list = wish.split(",")
     for gift in gifts:
-        if not (gift in formatedWishlist) and gift != "":
+        if not (gift in formatedWishlist):
             formatedWishlist.append(gift)
 
 for index, gift in enumerate(formatedWishlist):
-    if gift in history:
+    if str(gift).lstrip() in str(history).lstrip():
         formatedWishlist.pop(index)
 
 giftList = history + formatedWishlist
 
+tempGiftList = []
+
+for index, gift in enumerate(giftList):
+    print(gift)
+    giftList[index] = str(gift).lstrip()
+    if gift == "":
+        giftList.pop(index)
+    if gift in tempGiftList:
+        giftList.pop(index)
+    else:
+        tempGiftList.append(gift)
+
 print(giftList)
 
-# cur.execute("CREATE TABLE IF NOT EXISTS gifts (gift, age_limit, category);")
+# get age limits and categorys
+cur.execute("CREATE TABLE IF NOT EXISTS gifts (gift, age_limit, category);")
 
-# for gift in giftList:
-#     print("\n" + gift)
-#     while True:
-#         try:
-#             ageLimit = int(input("enter age limit $ "))
-#             break
-#         except:
-#             print("not a valid number\n")
-#     category = input("enter category $ ")
-#     con.execute("INSERT INTO gifts (gift, age_limit, category) VALUES (?, ?, ?)", [gift, ageLimit, category])
+for gift in giftList:
+    print("\n" + gift)
+    while True:
+        try:
+            ageLimit = int(input("enter age limit $ "))
+            break
+        except:
+            print("not a valid number\n")
+    category = input("enter category $ ")
+    con.execute("INSERT INTO gifts (gift, age_limit, category) VALUES (?, ?, ?)", [gift, ageLimit, category])
     
-# con.commit()
+con.commit()
 
-# cur.execute("SELECT * FROM gifts;")
+cur.execute("SELECT * FROM gifts;")
 
-# print(cur.fetchall())
+print(cur.fetchall())
+
+# add gifts to gifts table
+cur.execute("CREATE TABLE IF NOT EXISTS gifts (gift, age_limit, category);")
+
+for gift in giftList:
+    print("\n" + gift)
+    while True:
+        try:
+            ageLimit: int = int(input("enter age limit $ "))
+            break
+        except:
+            print("not a valid number\n")
+    category = input("enter category $ ")
+    con.execute("INSERT INTO gifts (gift, age_limit, category) VALUES (?, ?, ?)", [gift, ageLimit, category])
+    
+con.commit()
+
+cur.execute("SELECT * FROM gifts;")
+
+print(cur.fetchall())
